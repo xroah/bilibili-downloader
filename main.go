48abf2bcffb8becdaf26e0601ec9dd3896c597ba
xroah <396069583@qq.com>
2022-07-11 11:24:54 +0800

    Initialize go

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "github.com/gofiber/fiber/v2"
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(ctx *fiber.Ctx) error {
+		return ctx.SendString("hello world")
+	})
+	app.Listen(":3000")
+}
