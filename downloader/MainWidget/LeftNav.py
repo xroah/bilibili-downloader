d696064438a1198eecb92ec4d9a261bc98165f25
xroah <396069583@qq.com>
2022-07-16 16:29:00 +0800

    toolbar and left nav

--- downloader/MainWidget/LeftNav.py
+++ downloader/MainWidget/LeftNav.py
@@ -1,4 +1,5 @@
 from PySide6 import QtWidgets
+from PySide6.QtCore import Qt
 
 
 class LeftNav(QtWidgets.QWidget):
@@ -7,13 +8,48 @@ class LeftNav(QtWidgets.QWidget):
         layout = QtWidgets.QVBoxLayout(parent)
         self.downloading_btn = QtWidgets.QPushButton(parent=self, text="正在下载")
         self.downloaded_btn = QtWidgets.QPushButton(parent=self, text="已下载")
+        self.downloading_btn.setProperty("class", "downloading-btn")
+        self.downloading_btn.setCursor(Qt.PointingHandCursor)
+        self.downloaded_btn.setCursor(Qt.PointingHandCursor)
         layout.addWidget(self.downloading_btn)
         layout.addWidget(self.downloaded_btn)
         layout.setContentsMargins(0, 0, 0, 0)
         layout.addStretch()
-        self.setStyleSheet(
-            """
-            background-color: red;
-            """
-        )
+        layout.setSpacing(0)
+        self.setAttribute(Qt.WA_StyledBackground)
         self.setLayout(layout)
+        self.set_qss()
+        self.downloading_btn.clicked.connect(self.downloading_btn_click)
+        self.downloaded_btn.clicked.connect(self.downloaded_btn_click)
+
+    def downloading_btn_click(self):
+        print("downloading btn")
+
+    def downloaded_btn_click(self):
+        print("downloaded btn")
+
+    def set_qss(self):
+        self.setStyleSheet("""
+            LeftNav {
+                background-color: #fff;
+                border-right: 1px solid rgba(0, 0, 0, .2);
+            }
+
+            QPushButton {
+                padding: 10px;
+                border: none;
+            }
+
+            QPushButton:hover {
+                background-color: rgba(13, 110, 253, .5);
+                color: #fff;
+            }
+
+            QPushButton.active {
+                background-color: red;
+            }
+
+            QPushButton.downloading-btn {
+                border-bottom: 1px solid #f0f0f0;
+            }
+        """)
--- downloader/MainWindow/MainWindow.py
+++ downloader/MainWindow/MainWindow.py
@@ -1,8 +1,15 @@
-from PySide6.QtWidgets import QMainWindow
 from PySide6.QtCore import QSize
 from PySide6.QtGui import QIcon
-from ..utils import utils
+from PySide6.QtWidgets import (
+    QMainWindow,
+    QToolBar,
+    QToolButton,
+    QWidget,
+    QSizePolicy
+)
+
 from ..MainWidget import MainWidget
+from ..utils import utils
 
 
 class MainWindow(QMainWindow):
@@ -13,5 +20,41 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Bilibili下载器")
         self.setWindowIcon(icon)
         self.setMinimumSize(QSize(1080, 720))
-        self.setStyleSheet("background-color: rgba(0, 0, 0, .1);")
+        self.set_toolbar()
         self.show()
+
+    def set_toolbar(self):
+        toolbar = QToolBar(self)
+        toolbar.setMovable(False)
+        add_btn = QToolButton()
+        menu_btn = QToolButton()
+        placeholder = QWidget()
+        placeholder.setProperty("class", "placeholder")
+        placeholder.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        toolbar.setObjectName("toolbar")
+        toolbar.addWidget(add_btn)
+        toolbar.addWidget(placeholder)
+        toolbar.addWidget(menu_btn)
+        toolbar.setStyleSheet("""
+            QToolBar#toolbar {
+                padding: 10px;
+                border-bottom: 1px solid rgba(0, 0, 0, .1);
+                background-color: #fff;
+            }
+            
+            .placeholder {
+                background-color: transparent;
+            }
+            
+            QToolButton {
+                width: 24px;
+                height: 24px;
+                background-color: blue;
+            }
+            
+            QToolButton:hover {
+                background-color: red;
+            }
+        """)
+
+        self.addToolBar(toolbar)
