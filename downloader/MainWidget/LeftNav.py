e9ce05a1be6971d437a4ebce8d6299f6c42cd5db
xroah <396069583@qq.com>
2022-07-17 20:19:18 +0800

    nav change signal

--- downloader/MainWidget/LeftNav.py
+++ downloader/MainWidget/LeftNav.py
@@ -1,16 +1,20 @@
-from PySide6 import QtWidgets
-from PySide6.QtCore import Qt
+from PySide6.QtWidgets import (
+    QPushButton,
+    QWidget,
+    QVBoxLayout
+)
+from PySide6.QtCore import Qt, Signal
 
 
-class LeftNav(QtWidgets.QWidget):
-    def __init__(self, parent: QtWidgets.QWidget = None):
+class LeftNav(QWidget):
+    changed = Signal(QPushButton)
+
+    def __init__(self, parent: QWidget = None):
         super().__init__(parent)
-        layout = QtWidgets.QVBoxLayout(parent)
-        self.downloading_btn = QtWidgets.QPushButton(parent=self, text="正在下载")
-        self.downloaded_btn = QtWidgets.QPushButton(parent=self, text="已下载")
-        self.downloading_btn.setProperty("class", "downloading-btn")
-        self.downloading_btn.setCursor(Qt.PointingHandCursor)
-        self.downloaded_btn.setCursor(Qt.PointingHandCursor)
+        self.current: QPushButton | None = None
+        layout = QVBoxLayout(parent)
+        self.downloading_btn = self.create_btn("downloading-btn", "正在下载")
+        self.downloaded_btn = self.create_btn("downloaded-btn", "已下载")
         layout.addWidget(self.downloading_btn)
         layout.addWidget(self.downloaded_btn)
         layout.setContentsMargins(0, 0, 0, 0)
@@ -22,11 +26,34 @@ class LeftNav(QtWidgets.QWidget):
         self.downloading_btn.clicked.connect(self.downloading_btn_click)
         self.downloaded_btn.clicked.connect(self.downloaded_btn_click)
 
+    def create_btn(self, classname: str, text: str) -> QPushButton:
+        btn = QPushButton(parent=self, text=text)
+        btn.setProperty("class", classname)
+        btn.setCursor(Qt.PointingHandCursor)
+
+        return btn
+
+    def switch_tab(self, btn: QPushButton):
+        if self.current:
+            if self.current == btn:
+                return
+
+            self.current.setStyleSheet("")
+
+        self.current = btn
+        btn.setStyleSheet("""
+            background-color: rgba(13, 110, 253, .8);
+            color: #fff;
+        """)
+        self.changed.emit(btn)
+
     def downloading_btn_click(self):
         print("downloading btn")
+        self.switch_tab(self.downloading_btn)
 
     def downloaded_btn_click(self):
         print("downloaded btn")
+        self.switch_tab(self.downloaded_btn)
 
     def set_qss(self):
         self.setStyleSheet("""
--- downloader/MainWidget/MainWidget.py
+++ downloader/MainWidget/MainWidget.py
@@ -1,22 +1,28 @@
-import PySide6.QtWidgets as QtWidgets
+from PySide6.QtWidgets import (
+    QWidget,
+    QLabel,
+    QHBoxLayout,
+    QPushButton
+)
 from .LeftNav import LeftNav
 
 
-class MainWidget(QtWidgets.QWidget):
-    def __init__(self, parent: QtWidgets.QWidget = None):
+class MainWidget(QWidget):
+    def __init__(self, parent: QWidget = None):
         super().__init__(parent)
         left_nav = LeftNav(self)
-        right = QtWidgets.QLabel(self)
+        right = QLabel(self)
         right.setText("列表。。。")
         right.setStyleSheet("background-color: #fff;")
-        layout = QtWidgets.QHBoxLayout()
+
+        layout = QHBoxLayout()
         layout.addWidget(left_nav, 1)
         layout.addWidget(right, 5)
         layout.setSpacing(0)
         layout.setContentsMargins(0, 0, 0, 0)
-
         self.setLayout(layout)
 
-    def handle_click(self):
-        print("click")
-        self.button.setStyleSheet("")
+        left_nav.changed.connect(self.handle_tab_change)
+
+    def handle_tab_change(self, btn: QPushButton):
+        print("change", btn.property("class"))
