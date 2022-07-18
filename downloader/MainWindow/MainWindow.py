92ca27624f9c273f56e1c070614b325787cc6fd5
xroah <396069583@qq.com>
2022-07-18 16:45:52 +0800

    update

--- downloader/MainWindow/MainWindow.py
+++ downloader/MainWindow/MainWindow.py
@@ -1,14 +1,17 @@
-from PySide6.QtCore import QSize
-from PySide6.QtGui import QIcon, QPixmap
+from PySide6.QtCore import QSize, Qt
+from PySide6.QtGui import QIcon, QPixmap, QAction
 from PySide6.QtWidgets import (
     QMainWindow,
     QToolBar,
     QToolButton,
     QWidget,
-    QSizePolicy
+    QSizePolicy,
+    QMenu
 )
+import sys
 
 from ..MainWidget import MainWidget
+from ..utils import utils
 import downloader.QRC.Icons
 
 
@@ -24,10 +27,42 @@ class MainWindow(QMainWindow):
         self.add_btn.setIcon(QIcon(QPixmap(":/plus.png")))
         self.menu_btn.setIcon(QIcon(QPixmap(":/menu.png")))
         self.set_toolbar()
+        self.set_menu(self.menu_btn)
         self.show()
 
     def set_menu(self, btn: QToolButton):
-        pass
+        menu = QMenu(self)
+        about_action = menu.addAction("关于")
+        exit_action = menu.addAction("退出")
+        about_action.triggered.connect(self.about_action)
+        exit_action.triggered.connect(self.exit_action)
+        btn.setPopupMode(QToolButton.InstantPopup)
+        btn.setMenu(menu)
+        menu.setStyleSheet("""
+            QMenu {
+                padding: 0;
+                border-radius: 5px;
+                border: 1px solid #f0f0f0; 
+                background-color: #fff;
+            }
+            
+            QMenu::item {
+                margin: 0;
+                min-width: 150px;
+                padding: 10px;
+            }
+            
+            QMenu::item:selected {
+                color: #fff;
+                background-color: rgba(13, 110, 253, .6);
+            }
+        """)
+
+    def about_action(self):
+        print("about")
+
+    def exit_action(self):
+        sys.exit(0)
 
     def set_toolbar(self):
         toolbar = QToolBar(self)
@@ -39,23 +74,9 @@ class MainWindow(QMainWindow):
         toolbar.addWidget(self.add_btn)
         toolbar.addWidget(placeholder)
         toolbar.addWidget(self.menu_btn)
-        toolbar.setStyleSheet("""
-            QToolBar#toolbar {
-                padding: 5px;
-                border-bottom: 1px solid rgba(0, 0, 0, .1);
-                background-color: #fff;
-            }
-            
-            .placeholder {
-                background-color: transparent;
-            }
-            
-            QToolButton {
-                width: 24px;
-                height: 24px;
-            }
-            
-            
-        """)
+        toolbar.setContextMenuPolicy(Qt.PreventContextMenu)
+
+        with open(utils.get_resource_path("styles/toolbar.qss")) as ss:
+            toolbar.setStyleSheet(ss.read())
 
         self.addToolBar(toolbar)
--- downloader/utils/utils.py
+++ downloader/utils/utils.py
@@ -6,6 +6,6 @@ import os
 def get_resource_path(resource: str):
     file = inspect.getfile(__main__)
     dir_name = os.path.dirname(file)
-    file_path = os.path.normpath(os.path.join(dir_name, "resources", resource))
+    file_path = os.path.normpath(os.path.join(dir_name, resource))
 
     return file_path
