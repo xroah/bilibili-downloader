7691ceeaf5c405d98b26ef4bc766bc38114c3056
xroah <396069583@qq.com>
2023-02-05 11:11:17 +0800

    remove PySide6

--- downloader/common_widgets/__init__.py
+++
@@ -1,7 +0,0 @@
-from .PushButton import PushButton
-from .Dialog import Dialog
-from .Input import Input
-from .Menu import Menu
-from .MessageBox import MessageBox
-from .ClickableWidget import ClickableWidget
-from .Loading import Loading
--- downloader/main_widget/__init__.py
+++
@@ -1,5 +0,0 @@
-from .DownloadedTab import DownloadedTab
-from .DownloadingTab import DownloadingTab
-from .DownloadingItem import DownloadingItem
-from .DownloadedItem import DownloadedItem
-
