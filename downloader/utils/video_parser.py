004720fcaa03ee3d018a0d7dbd0e5e526de59212
xroah <396069583@qq.com>
2023-02-06 22:47:56 +0800

    parse episodes

--- downloader/utils/video_parser.py
+++ downloader/utils/video_parser.py
@@ -1,8 +1,15 @@
 import re
 import os.path
 from urllib.parse import urlparse
+import html
+import json
+from bs4 import BeautifulSoup
+
 from .request import get
 from ..enums import Req
+
+_info_prefix = "window.__playinfo__="
+_state_prefix = "window.__INITIAL_STATE__="
 
 
 def match_video_id(vid: str) -> bool:
@@ -41,12 +48,56 @@ def parse_url(url: str) -> str | None:
     return None
 
 
-def get_html(url: str):
-    vid = parse_url(url)
-
-    if vid:
-        res = get(f"{Req.REFERER}/video/{vid}")
-
-        return res.text
+def get_episodes(bvid: str):
+    ret = {
+        "episodes": [],
+        "code": 0,
+        "msg": ""
+    }
+    try:
+        res = get(f"{Req.REFERER}/video/{bvid}")
+    except:
+        ret["code"] = -1
+        ret["msg"] = "请求发生错误"
     else:
-        return None
+        soup = BeautifulSoup(res.text, "html.parser")
+        scripts = soup.select("script")
+
+        for s in scripts:
+            text = s.string
+
+            if not text:
+                continue
+
+            text = re.sub(r"\s+", "", text.strip())
+            text = html.unescape(text)
+
+            if text.startswith(_state_prefix):
+                text = text.replace(_state_prefix, "")
+                # remove js code
+                text = re.sub(r";[\(\)]function.*", "", text)
+                state = json.loads(text)
+                if len(state.get("sections", [])):
+                    sections = state["sections"]
+
+                    for sec in sections[0]["episodes"]:
+                        ret["episodes"].append({
+                            "aid": sec["aid"],
+                            "bvid": sec["bvid"],
+                            "cid": sec["cid"],
+                            "title": sec["title"]
+                        })
+                elif "videoData" in state:
+                    video_data = state["videoData"]
+                    pages = video_data["pages"]
+
+                    for p in pages:
+                        ret["episodes"].append({
+                            "aid": video_data["aid"],
+                            "bvid": video_data["bvid"],
+                            "cid": p["cid"],
+                            "title": p["part"]
+                        })
+                break
+
+    return ret
---
+++ main.py
@@ -0,0 +1,4 @@
+from downloader.utils.video_parser import get_episodes
+
+if __name__ == "__main__":
+    print(get_episodes("BV1wD4y1o7AS"))
