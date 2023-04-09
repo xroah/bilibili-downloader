10a7b47d0417130d0c97ddbdbc29407f89995939
xroah <396069583@qq.com>
2023-04-09 21:08:16 +0800

    feat: cookie cmd

--- downloader/cli/cli.py
+++ downloader/cli/cli.py
@@ -1,4 +1,18 @@
 import argparse
+from ..cookie import cookie
+
+
+def _handle_cmd(args: argparse.Namespace):
+    if hasattr(args, "bvid"):
+        # params = "avid=712020469&bvid=BV1wD4y1o7AS&cid=234725733&fnval=16&fnver=0&fourk=1&gaia_source=&qn=0"
+        print(args.bvid)
+    elif hasattr(args, "cookie"):
+        if args.d:
+            cookie.delete()
+        elif args.cookie is not None:
+            cookie.set(args.cookie)
+        else:
+            print(cookie.cookie)
 
 
 def parse():
@@ -7,10 +21,20 @@ def parse():
         description="Download videos from bilibili.com"
     )
     subparsers = parser.add_subparsers()
-    parser_d = subparsers.add_parser("download", aliases=["d"])
 
-    parser_d.add_argument("bvid")
+    download_parser = subparsers.add_parser("download", aliases=["d"])
+    download_parser.add_argument("bvid", nargs="+")
+
+    cookie_parser = subparsers.add_parser("cookie", aliases=["c"])
+    cookie_parser.add_argument("cookie", nargs="?")
+    cookie_parser.add_argument(
+        "-d",
+        "--delete",
+        action="store_true",
+        dest="d"
+    )
 
     args = parser.parse_args()
+    _handle_cmd(args)
 
     return args
--- main.py
+++ main.py
@@ -6,16 +6,7 @@ from downloader.enums import Req
 
 
 if __name__ == "__main__":
-    args = cli.parse()
+    args = vars(cli.parse())
 
-    if not hasattr(args, "bvid"):
+    if len(args.keys()) == 0:
         run_server()
-    else:
-        # params = "avid=712020469&bvid=BV1wD4y1o7AS&cid=234725733&fnval=16&fnver=0&fourk=1&gaia_source=&qn=80"
-        # final_params = encrypt(params)
-        # res = get(str(Req.PLAY_URL) + "?" + final_params)
-
-        # print(res.json())
-        res = get(str(Req.VIEW_URL) + "?" + encrypt("bvid=BV1r54y1m7gd"))
-
-        print(res.json())
